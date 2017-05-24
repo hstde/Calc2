d89ceca897f7de175baf23711985459db7f3b5bb
hstde <kranz.chr@googlemail.com>
2017-05-24 09:23:28 +0200

    Implement Math.Round() and Math.Int()

---
+++ Base/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcLang;
+using CalcLang.Interpreter;
+
+namespace Base
+{
+    internal static class MathLib
+    {
+        [CalcCallableMethod("__Int", 1)]
+        public static object Int(ScriptThread thread, object thisRef, object[] parameters) => (long)Convert.ToDecimal(parameters[0]);
+
+        [CalcCallableMethod("__Round", 1)]
+        public static object Round(ScriptThread thread, object instance, object[] parameters) => (decimal)Math.Round(Convert.ToDecimal(parameters[0]));
+    }
+}
