3fa2801a051f3441f68007e404437122e04384b4
hstde <kranz.chr@googlemail.com>
2017-06-13 15:21:43 +0200

    Fixed parsing of empty instructions
    parser will now correctly parse
    for(;;);

---
+++ CalcLang/Ast/EmptyNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcLang.Interpreter;
+
+namespace CalcLang.Ast
+{
+    public class EmptyNode : AstNode
+    {
+        protected override object DoEvaluate(ScriptThread thread) => thread.Runtime.NullValue;
+    }
+}
